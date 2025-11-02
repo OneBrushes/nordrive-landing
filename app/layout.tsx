263cf15de88c957424960a82cd5032ae5001646d
OneBrushes <onebrushes@gmail.com>
2025-11-02 12:32:51 +0100

    Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
+import Script from 'next/script'
 import './globals.css'
 import { TallyModalProvider } from "@/contexts/tally-modal-context"
 
@@ -26,6 +27,15 @@ export default function RootLayout({
           {children}
         </TallyModalProvider>
         <Analytics />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "tzsebm8lb5");
+          `}
+        </Script>
       </body>
     </html>
   )
